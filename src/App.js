3849a43a6d44d4f18fab854f2e6714d0a5e94fed
Brixi <denikucevic@gmail.com>
2020-12-04 18:21:52 +0100

    changed colors

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from "react";
 import "./App.css";
 import MainPage from "./pages/MainPage";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <MainPage />
     </div>
   );
-}
+};
 
 export default App;
